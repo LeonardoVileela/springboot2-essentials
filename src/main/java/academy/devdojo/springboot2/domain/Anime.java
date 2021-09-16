e41503aa7b4e7026dfab7a779d1b6994d8145360
LeonardoVileela <leovilela.empresa@gmail.com>
2021-09-16 20:39:38 -0300

    first

---
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -0,0 +1,26 @@
+package academy.devdojo.springboot2.domain;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@RequiredArgsConstructor
+@Entity
+public class Anime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Anime(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
